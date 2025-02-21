90c7b1f0b88aba95fdfc8e0fa2cd2aad09e02e1a
abdurraihan <abdurraihan898@gmail.com>
2025-02-22 02:15:15 +0600

    create pages and add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
 
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
 export default App;
