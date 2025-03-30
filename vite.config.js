acb7fe3ba047ced21f2ca7a00790fa12fcb28a96
abdurraihan <abdurraihan898@gmail.com>
2025-03-30 06:24:12 +0600

    fix config file

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,20 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd()); // Load environment variables
+  const env = loadEnv(mode, process.cwd());
 
   return {
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_API_URL, // Use the API URL from .env
+          target: env.VITE_API_URL,
           changeOrigin: true,
           ws: true,
         },
       },
     },
     define: {
-      "process.env": env, // Ensure environment variables are available globally
+      "process.env": env,
     },
     plugins: [tailwindcss()],
   };
