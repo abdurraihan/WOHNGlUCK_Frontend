1d704de6c4722ad0aa7aa335b4f5a8474f6dcc3a
abdurraihan <abdurraihan898@gmail.com>
2025-03-30 06:07:57 +0600

    fix to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://wohngluk-api.onrender.com",
-        changeOrigin: true,
-        ws: true,
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); // Load environment variables
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL, // Use the API URL from .env
+          changeOrigin: true,
+          ws: true,
+        },
       },
     },
-  },
-  plugins: [tailwindcss()],
+    define: {
+      "process.env": env, // Ensure environment variables are available globally
+    },
+    plugins: [tailwindcss()],
+  };
 });
